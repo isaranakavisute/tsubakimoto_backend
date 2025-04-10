6132c676e92b8ead6c0dfb9afecc2058a1d84a52
Isara Nakavisute <isara_nakavisute@hotmail.com>
2025-04-10 10:26:52 +0700

    add connection.end to mysql connection

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ async function query(sql, params) {
   //const connection = await mysql.createConnection(config.db);
   const connection = await mysql.createPool(config.db);
   const [results, ] = await connection.execute(sql, params);
-
+  connection.end();
   return results;
 }
 
